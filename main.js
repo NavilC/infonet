97cc9492df467c83224636064027ce8a3b60015b
ancoz98 <navil.cacerez@hotmail.com>
2025-01-28 19:28:14 -0600

    commit inicial
    inicial commit

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-var map = L.map('map').setView([51.505, -0.09], 13); //direccion
+var map = L.map('map').setView([15.5595391, -88.0061586], 16); //direccion
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 15,
+    maxZoom: 20,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09], 18).addTo(map); //efecto de donde es y el mensaje
+var marker = L.marker([15.5595391, -88.0061586], 16).addTo(map); //efecto de donde es y el mensaje
 marker.bindPopup("<b>Hello, I'm Infonet.!</b><br>I am the best company.").openPopup();
+
